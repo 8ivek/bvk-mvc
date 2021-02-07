cee5af05e29c3b6ead877adcc94b43a9f1f9030c
Bivek <bivek.joshi@mercatus.com>
2021-02-07 00:12:43 -0500

    route added.

--- Core/Router.php
+++ Core/Router.php
@@ -10,6 +10,11 @@ class Router
     protected array $routes = []; // Todo: make routes type with attribute route and params
 
     /**
+     * @var array
+     */
+    private array $params;
+
+    /**
      * Add a route to the routing table
      *
      * @param string $route The route URL
@@ -28,5 +33,31 @@ class Router
     public function getRoutes(): array
     {
         return $this->routes;
+    }
+
+    /**
+     * Match the route to the routes in the routing table, setting the property if a route is found.
+     *
+     * @param string $url the route url
+     * @return bool true if match is found, false otherwise
+     */
+    public function match(string $url): bool
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url === $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the currently matched parameters
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
     }
 }
--- public/index.php
+++ public/index.php
@@ -9,7 +9,22 @@ $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 
-// display this routing table
-echo "<pre>";
-var_dump($router->getRoutes());
-echo "</pre>";
+// match the requested route
+$url = '';
+if (!empty($_SERVER['QUERY_STRING'])) {
+    $url = $_SERVER['QUERY_STRING'];
+} elseif (!empty($_SERVER['REQUEST_URI'])) {
+    $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+    if (!file_exists(__DIR__ . '/' . $uri) && !is_file(__DIR__ . '/' . $uri)) {
+        $url = trim($uri, '/');
+    }
+}
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for url: $url";
+}
