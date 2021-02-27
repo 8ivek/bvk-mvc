d0f62fc86b880391e456511799fdd5ae62b2b311
8ivek <bivek_j@yahoo.com>
2021-02-28 05:09:03 +0545

    Views

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,22 +3,44 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use Core\View;
 
 class Home extends Controller
 {
+    /**
+     * calling url: https://bvkmvc.test/home/index
+     */
     public function indexAction()
     {
-        echo 'Hello from the index action in the Home controller!';
+        //echo 'Hello from the index action in the Home controller!';
+        View::render('Home/index.php');
+    }
+
+    /**
+     * calling url: https://bvkmvc.test/home/output-escaping
+     */
+    public function OutputEscapingAction()
+    {
+        View::render('OuputEscaping/index.php');
+    }
+
+    /**
+     * calling url: https://bvkmvc.test/home/pass-variables
+     */
+    public function PassVariablesAction()
+    {
+        $data = ['name' => 'Bivek', 'colors' => ['red', 'green', 'blue']];
+        View::render('PassVariables/index.php', $data);
     }
 
     protected function before()
     {
-        echo "(before) ";
+        //echo "(before) ";
         // return false; // useful for things like login check
     }
 
     protected function after()
     {
-        echo " (after)";
+        //echo " (after)";
     }
 }
